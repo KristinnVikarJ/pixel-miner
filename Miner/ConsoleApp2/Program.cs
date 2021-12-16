63586a0ab53df7fd9865b8ed6b7481d354a658ea
Kristinn Vikar <kristinn@minecraftpalace.com>
2021-12-16 02:19:14 +0000

    init

---
+++ Miner/ConsoleApp2/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Net.Http;
+using System.Threading;
+using System.Diagnostics;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        struct WorkData
+        {
+            public string session;
+            public string salt;
+            public string target;
+        }
+
+        struct FinishData
+        {
+            public string session;
+            public string pow;
+        }
+
+        static ConcurrentQueue<WorkData> workQueue;
+        static ConcurrentQueue<FinishData> finishQueue;
+
+        public static void SenderThread()
+        {
+            HttpClient c = new HttpClient();
+            while (true)
+            {
+                if (!finishQueue.IsEmpty && finishQueue.TryDequeue(out FinishData finishData))
+                {
+                    var parameters = new Dictionary<string, string> { { "session", finishData.session }, { "pow", finishData.pow } };
+                    var data = new FormUrlEncodedContent(parameters);
+                    c.PostAsync("https://piebot.xyz/ctf/pixels/data", data);
+                }
+                else
+                {
+                    Thread.Sleep(5);
+                }
+            }
+        }
+
+        public static void WorkerThread()
+        {
+            while (true)
+            {
+                if(!workQueue.IsEmpty && workQueue.TryDequeue(out WorkData data))
+                {
+                    Console.WriteLine(workQueue.Count);
+                    Process p = Process.Start("md5.exe", data.salt + " " + data.target);
+                    p.StartInfo.CreateNoWindow = true;
+                    p.StartInfo.UseShellExecute = false;
+                    p.StartInfo.RedirectStandardOutput = true;
+                    p.Start();
+                    string msg = p.StandardOutput.ReadToEnd();
+                    finishQueue.Enqueue(new FinishData()
+                    {
+                        pow = msg,
+                        session = data.session
+                    });
+                }
+                else
+                {
+                    Thread.Sleep(5);
+                }
+            }
+        }
+
+        public static async void FetcherThread()
+        {
+            var httpClientHandler = new HttpClientHandler
+            {
+                UseCookies = false,
+                AllowAutoRedirect = false,
+            };
+            HttpClient c = new HttpClient(httpClientHandler);
+            while (true)
+            {
+                if (workQueue.Count < 20)
+                {
+                    var parameters = new Dictionary<string, string> { { "action", "get_work" } };
+                    var data2 = await c.PostAsync("http://challs.xmas.htsp.ro:3002/api", new FormUrlEncodedContent(parameters));
+                    string content = await data2.Content.ReadAsStringAsync();
+
+                    string[] temp = content.Split(' ');
+
+                    try
+                    {
+                        string session = ((string[])data2.Headers.GetValues("Set-Cookie"))[0];
+                        string target = temp[^1];
+
+                        workQueue.Enqueue(new WorkData()
+                        {
+                            session = session,
+                            salt = temp[3].Substring(0, 64),
+                            target = target
+                        });
+                        Thread.Sleep(150);
+                    }
+                    catch { }
+                }
+                else
+                {
+                    Thread.Sleep(50);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            workQueue = new ConcurrentQueue<WorkData>();
+            finishQueue = new ConcurrentQueue<FinishData>();
+
+            Thread workerThread = new Thread(WorkerThread);
+            workerThread.Start();
+
+            Thread fetcherThread = new Thread(FetcherThread);
+            fetcherThread.Start();
+
+            Thread senderThread = new Thread(SenderThread);
+            senderThread.Start();
+
+            while (true)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ Sender/ConsoleApp3/Program.cs
@@ -0,0 +1,155 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        struct SuperData
+        {
+            public string salt;
+            public byte[] target;
+            public string session;
+        }
+
+        struct Pos
+        {
+            public int X;
+            public int Y;
+        }
+
+        struct DrawData
+        {
+            public Pos pos;
+            public string color;
+            public string session;
+            public string pow;
+        }
+
+        static ConcurrentQueue<SuperData> queue;
+        public static string URL;
+
+        static async Task<SuperData> GetWork()
+        {
+            HttpClient c = new HttpClient();
+            var parameters = new Dictionary<string, string> { { "action", "get_work" } };
+            var data = new FormUrlEncodedContent(parameters);
+            var data2 = await c.PostAsync("http://challs.xmas.htsp.ro:3002/api", data);
+            string content = await data2.Content.ReadAsStringAsync();
+
+            string[] temp = content.Split(' ');
+
+            string session = ((string[])data2.Headers.GetValues("Set-Cookie"))[0];
+            string target = temp[temp.Length-1];
+            byte[] bytes = new byte[target.Length / 2];
+
+            for (int i = 0; i < target.Length; i += 2)
+                bytes[i / 2] = Convert.ToByte(target.Substring(i, 2), 16);
+
+            return new SuperData()
+            {
+                session = session,
+                salt = temp[3].Substring(0, 64),
+                target = bytes
+            };
+        }
+
+        public static string GetRandomHexColor()
+        {
+            var result = Guid.NewGuid().ToString().Substring(0, 6);
+            return result;
+        }
+
+        static ConcurrentQueue<DrawData> PostQueue = new ConcurrentQueue<DrawData>();
+
+        public static async void PosterThread()
+        {
+            var httpClientHandler = new HttpClientHandler
+            {
+                UseCookies = false,
+                AllowAutoRedirect = false,
+            };
+            HttpClient c = new HttpClient(httpClientHandler);
+            DrawData drawData = new DrawData();
+            bool Finished = true;
+            while (true)
+            {
+                if (!Finished || (!PostQueue.IsEmpty && PostQueue.TryDequeue(out drawData)))
+                {
+                    Stopwatch stopwatch = new Stopwatch();
+                    stopwatch.Start();
+                    Console.WriteLine($"drawing at x:{drawData.pos.Y.ToString()} y:{drawData.pos.X.ToString()} color:{drawData.color}");
+                    var parameters = new Dictionary<string, string> { { "action", "paint" },
+                    { "team", "42" },
+                    { "x", drawData.pos.Y.ToString() },
+                    { "y", drawData.pos.X.ToString() },
+                    { "color", drawData.color },
+                    { "work", drawData.pow } 
+                    };
+                    var data = new FormUrlEncodedContent(parameters);
+                    data.Headers.Add("Cookie", drawData.session);
+                    var data2 = await c.PostAsync("http://challs.xmas.htsp.ro:3002/api", data);
+                    stopwatch.Stop();
+                    string p = await data2.Content.ReadAsStringAsync();
+                    if(p == "You haven't POSTed with action=get_work first!")
+                    {
+                        Console.WriteLine("session got invalidated, refreshing");
+                        Finished = true;
+                        Thread.Sleep(50);
+                        continue;
+                    }
+                    if (data2.StatusCode != HttpStatusCode.OK)
+                    {
+                        Finished = false;
+                        Thread.Sleep(50);
+                        continue;
+                    }
+                    Thread.Sleep(220);
+                }
+            }
+        }
+
+        public static async void GetterThread()
+        {
+            HttpClient c = new HttpClient();
+            while (true)
+            {
+                Thread.Sleep(100);
+                if (PostQueue.Count < 30)
+                {
+                    var data2 = await c.GetAsync("https://piebot.xyz/ctf/pixels/get");
+                    string jsonData2 = await data2.Content.ReadAsStringAsync();
+                    if (jsonData2 == "{}")
+                        continue;
+                    foreach (DrawData i in JsonConvert.DeserializeObject<DrawData[]>(jsonData2))
+                    {
+                        PostQueue.Enqueue(i);
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            queue = new ConcurrentQueue<SuperData>();
+            //PostQueue = new ConcurrentQueue<DrawData>();
+            Thread t2 = new Thread(GetterThread);
+            t2.Start();
+            Thread t3 = new Thread(PosterThread);
+            t3.Start();
+            while (true)
+            {
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
